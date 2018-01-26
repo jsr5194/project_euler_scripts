474abc3bac5fdaf5f2919c4db96bd4ddbc2216f3
debian <debian@debian>
2018-01-26 15:23:34 -0500

    update 3 with solution to the example but not solution

--- asm/3.s
+++ asm/3.s
@@ -9,21 +9,20 @@ _start:
 	; set up space for the divisor
 	; location ebp-4
 	xor eax, eax
-	mov ax, 0x338b
+	mov ax, 0x338b ;13195
+	push eax
 	
 	; set up space to hold highest prime
 	; location ebp-8
 	xor eax, eax
 	push eax
 
-	; initialize the divisor
-	mov eax, [ebp-4] 
-
 	; initialize counter to first possible value
 	xor ecx, ecx
 	mov cl, 0x1
 
 findfactor:
+	mov eax, [ebp-4]
 	add ecx, 0x2 ;dont need to look at evens
 	xor edx, edx
 	div ecx
@@ -34,8 +33,56 @@ findfactor:
 	jmp findfactor
 
 testprime:
-	nop	
-
-
+	mov eax, ecx
+	xor ecx, ecx
+	add ecx, 0x2
+testprimeloop:
+	push eax
+	xor edx, edx
+	div ecx
+	test edx, edx
+	pop eax        ;get the findfactor counter back
+	mov ebx, ecx   ;temporarially store the testprime counter
+	mov ecx, eax   ;put the findfactor counter in the right place for if we jump
+	jz findfactor  ;jump if it finds a whole divide
+	mov ecx, ebx   ;fix counters in case that jump didnt happen
+	inc ecx
+	cmp ecx, eax
+	jnz testprimeloop
+	mov [ebp-8], eax
+	mov ecx, eax
+	jmp findfactor
+	
 printprime:
-	nop
+	xor eax, eax
+	push eax ;null terminate string
+	push 0xa ;push newline
+	mov eax, [ebp-8]
+itosloop:
+	xor edx, edx
+	mov ecx, edx
+	mov cl, 0xa
+	div ecx
+	add edx, 0x30
+	push edx
+	test eax, eax
+	jnz itosloop
+printloop:
+	xor eax, eax
+	mov ebx, eax
+	mov edx, eax
+	mov al, 0x4
+	mov bl, 0x1
+	lea ecx, [esp]
+	mov dl, 0x1
+	int 0x80
+	pop ecx
+	test ecx, ecx
+	jnz printloop
+	
+
+exit:
+	xor eax, eax
+	mov al, 0x1
+	xor ebx, ebx
+	int 0x80
